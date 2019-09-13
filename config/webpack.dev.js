42d28391fc83d5d8cff2490ef57b156409f32a66
Teenu Rani <teerani@publicisgroupe.net>
2019-09-13 12:10:36 +0530

    webpack setup

---
+++ config/webpack.dev.js
@@ -0,0 +1,77 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: "./src/main.js"
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "dist",
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].html"
+            }
+          },
+          {
+            loader: "extract-loader"
+          },
+          {
+            loader: "html-loader",
+            options: {
+              attrs: ["img:src"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg)/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[.ext]"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.hbs$/,
+        use: [
+          {
+            loader: "handlebars-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
