2c9d9a4d53ff43d2327e297473eabcc5eec4681d
Teenu Rani <teerani@publicisgroupe.net>
2019-09-19 17:01:23 +0530

    chnage in config

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,8 +7,7 @@ module.exports = {
   mode: "development",
   output: {
     filename: "[name]-bundle.js",
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "/"
+    path: path.resolve(__dirname, "../dist")   
   },
   devServer: {
     contentBase: "dist",
