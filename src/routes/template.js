5acd1ea8c4023ec21b3f9d6521fe7d4482f4c27d
Teenu Rani <teerani@publicisgroupe.net>
2019-10-29 11:41:59 +0530

    move constants

--- src/routes/template.js
+++ src/routes/template.js
@@ -1,20 +1,29 @@
 var express = require("express");
 var router = express.Router();
+var en = require("../i18/en.json");
 
 router.all("/", function(req, res, next) {
-  res.render("home");
+  res.render("home", { header: en.header, footer: en.footer, home: en.home });
 });
 
 router.get("/product", function(req, res, next) {
-  res.render("product");
+  res.render("product", { header: en.header, footer: en.footer });
 });
 
 router.get("/registration", function(req, res, next) {
-  res.render("registration");
+  res.render("registration", {
+    header: en.header,
+    footer: en.footer,
+    register: en.register
+  });
 });
 
 router.get("/login", function(req, res, next) {
-  res.render("login");
+  res.render("login", {
+    header: en.header,
+    footer: en.footer,
+    login: en.login
+  });
 });
 
 module.exports = router;
